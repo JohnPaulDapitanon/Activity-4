beb3af733360105e7af9b23a1538932707a082b6
JohnPaulDapitanon <70047221+JohnPaulDapitanon@users.noreply.github.com>
2022-10-20 09:54:44 +0800

    Array Filter

---
+++ js-fundamentals.js
@@ -0,0 +1,7 @@
+//Write a JavaScript function that accepts an array as parameter and  returns the elements of an array of numbers that are less than 18.
+
+const number = [ 30, 15, 12, 21, 19];
+
+const filtered= number.filter(n => n < 18 );
+
+console.log(filtered);
